a9eeecd0900e43859861065afb09e088b6afa3f3
anvarjon-programmer <mranvarjontojiboyevgmail.com>
2023-11-19 16:05:35 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import img from "../images/Logo.png";
+import { FaUser } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+export default function Header() {
+
+ const [list, setList]=useState(true);
+  console.log(list);
+
+
+  return (
+    <header>
+      <nav>
+        <div className="container">
+        <div className="navbar">
+          <img src={img} alt="" />
+          <ul className={list ? "navbar__list" : "out"}>
+            <li className="fa-x">
+              <button className="remove__btn"  onClick={()=>{setList(false); console.log(1)}}>
+              X
+              </button>
+              </li>
+            <li>
+              <a href="#">Marketplace</a>
+            </li>
+            <li>
+              <a href="#">Rankings</a>
+            </li>
+            <li>
+              <a href="#">Connect a wallet</a>
+            </li>
+            <li>
+              <button className="navbar__lsit__btn">
+              <FaUser className="faUser" />
+              Sign Up
+              </button>
+            </li>
+          </ul>
+          <RxHamburgerMenu className="burger" onClick={()=>setList(true)}/>
+
+        </div>
+        </div>
+      </nav>
+    </header>
+  );
+
+  
+}
+
+
